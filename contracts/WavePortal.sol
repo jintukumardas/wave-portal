d9f6e5b80e90712f3fe3d459a85bde36f34465ab
Jintu Kumar Das <jintukumardas@gmail.com>
2022-04-28 21:32:35 +0530

    added ether to contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,9 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     
+    constructor() payable {
+    }
+
     uint noOfWaves;
 
     event NewWaveAdded(address indexed from, uint time, string message);
@@ -22,6 +25,13 @@ contract WavePortal {
         noOfWaves++;
         waves.push(Wave(msg.sender, block.timestamp, _message));
         emit NewWaveAdded(msg.sender, block.timestamp, _message);
+        uint prizeAmount = 0.00001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,15 @@
 const main = async() => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.0001"),
+    });
     await waveContract.deployed();
     console.log("Wave Portal Deployed @ ",waveContract.address);
 }
 
 const runMain = async() => {
     try{
-
         await main();
         process.exit(0);
     }catch(e){
